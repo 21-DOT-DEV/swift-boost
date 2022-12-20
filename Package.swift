6d00754d68e67b5ca60e7b32bf6c18e3284355f8
Chris <csjones@users.noreply.github.com>
2022-12-19 23:48:38 -0800

    Adding more boost libraries

--- Package.swift
+++ Package.swift
@@ -7,10 +7,31 @@ let package = Package(
     name: "Boost.swift",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "variant",
-            targets: ["variant"]
-        ),
+        .library(name: "assert", targets: ["assert"]),
+        .library(name: "bind", targets: ["bind"]),
+        .library(name: "config", targets: ["config"]),
+        .library(name: "container_hash", targets: ["container_hash"]),
+        .library(name: "core", targets: ["core"]),
+        .library(name: "detail", targets: ["detail"]),
+        .library(name: "foreach", targets: ["foreach"]),
+        .library(name: "function", targets: ["function"]),
+        .library(name: "integer", targets: ["integer"]),
+        .library(name: "iterator", targets: ["iterator"]),
+        .library(name: "move", targets: ["move"]),
+        .library(name: "mpl", targets: ["mpl"]),
+        .library(name: "multi_index", targets: ["multi_index"]),
+        .library(name: "optional", targets: ["optional"]),
+        .library(name: "preprocessor", targets: ["preprocessor"]),
+        .library(name: "serialization", targets: ["serialization"]),
+        .library(name: "signals2", targets: ["signals2"]),
+        .library(name: "smart_ptr", targets: ["smart_ptr"]),
+        .library(name: "static_assert", targets: ["static_assert"]),
+        .library(name: "throw_exception", targets: ["throw_exception"]),
+        .library(name: "tuple", targets: ["tuple"]),
+        .library(name: "type_index", targets: ["type_index"]),
+        .library(name: "type_traits", targets: ["type_traits"]),
+        .library(name: "utility", targets: ["utility"]),
+        .library(name: "variant", targets: ["variant"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,6 +40,30 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "assert"),
+        .target(name: "bind"),
+        .target(name: "config"),
+        .target(name: "container_hash"),
+        .target(name: "core"),
+        .target(name: "detail"),
+        .target(name: "foreach"),
+        .target(name: "function"),
+        .target(name: "integer"),
+        .target(name: "iterator"),
+        .target(name: "move"),
+        .target(name: "mpl"),
+        .target(name: "multi_index"),
+        .target(name: "optional"),
+        .target(name: "preprocessor"),
+        .target(name: "serialization"),
+        .target(name: "signals2"),
+        .target(name: "smart_ptr"),
+        .target(name: "static_assert"),
+        .target(name: "throw_exception"),
+        .target(name: "tuple"),
+        .target(name: "type_index"),
+        .target(name: "type_traits"),
+        .target(name: "utility"),
         .target(name: "variant"),
     ]
 )
---
+++ Sources/signals2/src/signals2.cpp
@@ -0,0 +1,12 @@
+//
+//  signals2.cpp
+//  GigaBitcoin/Boost.swift
+//
+//  Copyright (c) 2022 GigaBitcoin LLC
+//  Distributed under the MIT software license
+//
+//  See the accompanying file LICENSE for information
+//
+
+
+//  This file only exists for automatic module generation
