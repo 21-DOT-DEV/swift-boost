fe2fb61be144995164f7a3efc776607421a191d2
Chris <csjones@users.noreply.github.com>
2022-12-21 00:02:14 -0800

    Adding more boost libs

---
+++ Sources/numeric_conversion/src/numeric_conversion.cpp
@@ -0,0 +1,12 @@
+//
+//  numeric_conversion.cpp
+//  GigaBitcoin/Boost.swift
+//
+//  Copyright (c) 2022 GigaBitcoin LLC
+//  Distributed under the MIT software license
+//
+//  See the accompanying file LICENSE for information
+//
+
+
+//  This file only exists for automatic module generation
