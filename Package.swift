61ae436c448f5a660b3a3c717562d89774a9e904
csjones <csjones@users.noreply.github.com>
2024-07-05 01:24:04 -0700

    Update to 1.81.0 with backport for mpl

--- Package.swift
+++ Package.swift
@@ -4,9 +4,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "Boost.swift",
+    name: "Boost",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "algorithm", targets: ["algorithm"]),
         .library(name: "array", targets: ["array"]),
         .library(name: "assert", targets: ["assert"]),
@@ -43,13 +42,7 @@ let package = Package(
         .library(name: "utility", targets: ["utility"]),
         .library(name: "variant", targets: ["variant"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "algorithm"),
         .target(name: "array"),
         .target(name: "assert"),
