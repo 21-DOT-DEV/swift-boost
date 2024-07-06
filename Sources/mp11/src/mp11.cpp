51f9f7836acfa1b50e2f21c77250bbe8fd257895
csjones <csjones@users.noreply.github.com>
2024-07-05 20:04:32 -0700

    Adding Boost mp11

---
+++ Sources/mp11/src/mp11.cpp
@@ -0,0 +1,12 @@
+//
+//  mp11.cpp
+//  GigaBitcoin/Boost.swift
+//
+//  Copyright (c) 2024 GigaBitcoin LLC
+//  Distributed under the MIT software license
+//
+//  See the accompanying file LICENSE for information
+//
+
+
+//  This file only exists for automatic module generation
